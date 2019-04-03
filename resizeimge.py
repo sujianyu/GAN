a27064f4f450dfaafdee147d6064595f8c0f6935
sujianyu <sujianyu@foxmail.com>
2019-04-03 15:19:41 +0800

    tinymind hanzi GAN

---
+++ resizeimge.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import os
+import os
+data_dir = "data"
+data_set = "hanzi"
+weight_new = 255
+height_new = 255
+
+output_path = os.path.join(data_dir,data_set + "_resize")
+if not os.path.isdir(output_path):
+    os.mkdir(output_path)
+
+directors = []
+image_path = os.path.join(data_dir,data_set)
+for dir in os.listdir(image_path):
+    #在新文件夹中创建标签目录
+    dir_new = os.path.join(output_path,dir)
+    if not os.path.isdir(dir_new):
+        os.mkdir(dir_new)
+    path = os.path.join(image_path,dir)
+    if os.path.isdir(path):
+        directors.append(path)
+
+filenames = []
+def convertjpg(jpgfile,resizefile,width=255,height=255):
+    img = Image.open(jpgfile)
+    try:
+        new_img = img.resize((width,height),Image.BILINEAR)
+        new_img.save(resizefile)
+    except Exception as e:
+        print(e)
+
+for dir in directors:
+    for filename in os.listdir(dir):
+        path = os.path.join(dir,filename)
+        if os.path.isfile(path):
+            filenames.append(path)
+for filename in filenames:
+    filename_resize = filename.split("\\")
+    filename_resize = os.path.join(filename_resize[2], filename_resize[3])
+    resizefile = os.path.join(output_path,filename_resize)
+    print(resizefile)
+    convertjpg(filename,resizefile)
--- testtfrecord.py
+++ testtfrecord.py
@@ -6,6 +6,7 @@ data_set = "hanzi"
 weight_new = 255
 height_new = 255
 
+
 output_path = os.path.join(data_dir,data_set + "_resize")
 if not tf.gfile.Exists(output_path):
     tf.gfile.MakeDirs(output_path)
@@ -29,17 +30,31 @@ for dir in directors:
         path = os.path.join(dir,filename)
         if os.path.isfile(path):
             filenames.append(path)
-print(filenames)
 
-with tf.Session() as sess:
 
+    tf.reset_default_graph()
     for filename in filenames:
+        with tf.Session() as sess:
 
-        image_data = tf.gfile.FastGFile(filename, "rb").read()
-        image_data_decode = tf.image.decode_jpeg(image_data)
-        image = sess.run(image_data_decode)
-        h, w, c = image.shape
-        assert c==1
-        image = image.reshape(h,w)
+            image_data = tf.gfile.FastGFile(filename, "rb").read()
+            image_data_decode = tf.image.decode_jpeg(image_data)
+            image = image_data_decode.eval()
+            image = tf.image.convert_image_dtype(image,dtype=tf.float32)
+            image_resize = tf.image.resize_images(image,[height_new,weight_new],0)
+            image_new = tf.image.convert_image_dtype(image_resize,dtype=tf.uint8)
+            image_new = tf.image.encode_jpeg(image_new)
+            filename_resize = filename.split("\\")
+            filename_resize = os.path.join(filename_resize[2],filename_resize[3])
+            #print(filename_resize)
+            image_file = tf.gfile.FastGFile(os.path.join(output_path,filename_resize),"w")
+            image_file.write(sess.run(image_new))
+            image_file.close()
+        tf.get_default_graph().finalize()
+        #print(filename_resize)
+        #ops = tf.get_default_graph().get_operations()
+        #print(len(ops))
+        # h, w, c = image.shape
+        # assert c==1
+        # image = image.reshape(h,w)
 
 
